909948003a1a1dbc208ff4217d75410387e43b44
Michal Dymek <m.dymek95@gmail.com>
2021-04-24 16:26:31 +0200

    add second script

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 var playerMove;
 playerMove = 'papier';
 printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
+
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else if (randomNumber == '2') {
+  computerMove = 'papier';
+} else if (randomNumber == '3') {
+  computerMove = 'nożyczki';
+}
+printMessage('Mój ruch: ' + computerMove);
