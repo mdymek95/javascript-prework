0290d84e2d6c2d5196e656a14c4446d67f752b08
Michal Dymek <m.dymek95@gmail.com>
2021-05-06 10:08:26 +0200

    change script language and operators

--- js/script.js
+++ js/script.js
@@ -1,8 +1,49 @@
-//var argButtonName, buttonPaper, buttonRock, buttonScissors, buttonTest;
-//var computerMove, randomNumber;
+
 const buttonPaper = document.getElementById('button-paper');
 const buttonRock = document.getElementById('button-rock');
 const buttonScissors = document.getElementById('button-scissors');
+
+buttonRock.addEventListener('click', function(){ buttonClicked('rock'); });
+buttonPaper.addEventListener('click', function(){ buttonClicked('paper'); });
+buttonScissors.addEventListener('click', function(){ buttonClicked('scissors'); });
+
+
+    function getMoveName(argMoveId) {
+    console.log('wywołano funkcję getMoveName z argumentem: ' + argMoveId);
+      if (argMoveId === 1) {
+      return 'rock';
+      } else if (argMoveId === 2) {
+        return 'paper';
+      } else if (argMoveId === 3) {
+        return 'scissors';
+      } else {
+      printMessage('Nie znam ruchu o id ' + argMoveId + '. Zakładam, że chodziło o "kamień".');
+      return 'kamień';
+      }
+    }
+
+
+    function displayResult(argPlayerMove, argComputerMove) {
+    console.log('wywołano funkcję displayResults z argumentami: ' + argPlayerMove + ', ' + argComputerMove);
+      if (argPlayerMove === 'paper' && argComputerMove === 'rock') {
+      printMessage('Wygrywasz!');
+      } 
+      else if (argPlayerMove === 'rock' && argComputerMove === 'scissors') {
+        printMessage('Wygrywasz!');
+      } 
+      else if (argPlayerMove === 'scissors' && argComputerMove === 'paper') {
+        printMessage('Wygrywasz!');
+      } 
+      else if (argPlayerMove === argComputerMove) {
+        printMessage('Remis!');
+      } 
+      else {
+        printMessage('Przegrywasz :(');
+      }
+        printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
+    }
+    
+
 
 function buttonClicked(argButtonName) {
   clearMessages();
@@ -16,56 +57,3 @@ function buttonClicked(argButtonName) {
   displayResult(playerMove, computerMove);
 
 }
-
-    //var argComputerMove, argMoveId, argPlayerMove, computerMove, playerInput, playerMove, randomNumber;
-
-
-    function getMoveName(argMoveId) {
-    console.log('wywołano funkcję getMoveName z argumentem: ' + argMoveId);
-      if (argMoveId == 1) {
-      return 'kamień';
-      } else if (argMoveId == 2) {
-        return 'papier';
-      } else if (argMoveId == 3) {
-        return 'nożyce';
-      } else {
-      printMessage('Nie znam ruchu o id ' + argMoveId + '. Zakładam, że chodziło o "kamień".');
-      return 'kamień';
-      }
-    }
-
-
-    function displayResult(argPlayerMove, argComputerMove) {
-    console.log('wywołano funkcję displayResults z argumentami: ' + argPlayerMove + ', ' + argComputerMove);
-      if (argPlayerMove == 'papier' && argComputerMove == 'kamień') {
-      printMessage('Wygrywasz!');
-      } 
-      else if (argPlayerMove == 'kamień' && argComputerMove == 'nożyce') {
-        printMessage('Wygrywasz!');
-      } 
-      else if (argPlayerMove == 'nożyce' && argComputerMove == 'papier') {
-        printMessage('Wygrywasz!');
-      } 
-      else if (argPlayerMove == argComputerMove) {
-        printMessage('Remis!');
-      } 
-      else {
-        printMessage('Przegrywasz :(');
-      }
-        printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
-    }
-    /*playerMove = argButtonName;
-    console.log('ruch gracza to: ' + playerMove);
-    randomNumber = Math.floor(Math.random() * 3 + 1);
-    console.log('wylosowana liczba to: ' + randomNumber);
-    computerMove = getMoveName(randomNumber);
-    console.log('ruch komputera to: ' + computerMove);
-    displayResult(playerMove, computerMove);*/
-
-
-
-
-
-buttonRock.addEventListener('click', function(){ buttonClicked('kamień'); });
-buttonPaper.addEventListener('click', function(){ buttonClicked('papier'); });
-buttonScissors.addEventListener('click', function(){ buttonClicked('nożyce'); });
